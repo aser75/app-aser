5e70cb1bb10785621a8d25ab4dc39da94be7fa3f
aser75 <max.puppetto@yahoo.com>
2017-09-20 11:39:56 +0200

    Animation items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule }               from '@angular/platform-browser';
 import { NgModule }                    from '@angular/core';
 import { FormsModule }                 from '@angular/forms';
+import { BrowserAnimationsModule }     from '@angular/platform-browser/animations';
 
 // Component
 import { AppComponent }                from './app.component';
@@ -21,6 +22,7 @@ import { AppRoutingModule }            from './app-routing.modules';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
+    BrowserAnimationsModule
   ],  
   declarations: [
     AppComponent,
--- src/app/projet.component.ts
+++ src/app/projet.component.ts
@@ -1,25 +1,47 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Projet } from './projet';
 import { ProjetService } from './projet.service';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector           : 'app-root',
   templateUrl        : './view/projet.component.html',
   styleUrls          : ['../assets/styl/view/projet.component.styl'],
-  providers          : [ProjetService]
+  providers          : [ProjetService],
+  animations         : [
+    trigger('itemMove', [
+      
+      state('in', style({transform: 'translateX(0)'})),
+      transition('void => *', [
+        style({transform: 'translateX(-100%)'}),
+        animate(100)
+      ]),
+      transition('* => void', [
+        animate(100, style({transform: 'translateX(100%)'}))
+      ])      
+    ])
+  ]
 })
 
+
 export class ProjetComponent implements OnInit, OnDestroy {
+
   title 	= 'App Aser';
   projets :  Projet[];
   selectedProjet: Projet;
 
   constructor (private projetService: ProjetService){}
 
+
+  /*
+    Service Projets
+  */
   getProjets(): void {
+
     this.projetService.getProjets().then(projets => this.projets = projets);
+
   }
- 
+
   ngOnInit(): void {
     
     this.getProjets();
